da83aa381e8da1a5659979b927a07bda5c8a3ef9
cheezel07 <cheezel.tin@ncfsolutionsinc.com>
2022-01-13 04:21:14 +0800

    deploy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
+import axios from "axios";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Hello() {
+  return <h2>Hello I am Cheeze!</h2>;
+}
+
+function Testpage() {
+  return (
+    <>
+	    <h1>Welcome to my page!</h1>
+      <Hello />
+      
+      <><div>
+        <button onClick={refreshPage}>Click to reload!</button>
+      </div>
+      </>
+	    
+    </>
+  );
+}
+
+
+async function fetchData1() {
+  
+  try {
+    const result = await axios.get("https://randomuser.me/api/");
+    console.log(result.data.results) ;
+    const data = result.data.results;
+    let name = data[0]['name']['title']+'. '+data[0]['name']['first']+' '+data[0]['name']['last'] ;
+    let email = data[0]['email'];
+   
+    localStorage.setItem('Name', name);
+    localStorage.setItem('Email', email);
+    console.log(name, email) ;
+    
+    
+    
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function refreshPage() {
+  window.location.reload(false);
+}
+
+
+fetchData1();
+
+ReactDOM.render(<Testpage />, document.getElementById('root'));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
